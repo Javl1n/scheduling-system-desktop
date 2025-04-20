aa3a051a412152b765ae72cf8715fbedc8b1bc23
Frank Leimbergh Armodia <farmodia@gmail.com>
2025-04-20 22:02:45 +0800

    added zoom functionality in room page

--- resources/js/components/rooms/stairs.tsx
+++ resources/js/components/rooms/stairs.tsx
@@ -1,56 +1,62 @@
-import { ArrowBigDown, ArrowDown, ArrowUp } from "lucide-react";
+import { ArrowBigDown, ArrowDown, ArrowLeft, ArrowRight, ArrowUp } from "lucide-react";
 import { Separator } from "../ui/separator";
 
-export default function Stairs() {
+export function CenterStairs() {
+     const stairs = () => {
+          return (
+               <>
+                    {Array.from({length: 5}, () => (
+                         <Separator />
+                    ))}
+               </>
+          )
+     }
+
      return (
           <div className="h-20 border-t-5 border-r-5 w-32">
-               <div className="grid grid-cols-4 h-15 pt-4">
+               <div className="grid grid-cols-4 h-full py-4">
                     <div className="grid gap-2 py-1 border-r-2">
-                         {/* <ArrowDown className="absolute ms-1 mt-1 stroke-border"/> */}
-                         <div className="absolute w-7 pt-1">
-                              <div className="border-2 w-px h-11 mx-auto dark:border-white">
-                              </div>
-                         </div>
-                         <Separator />
-                         <Separator />
-                         <Separator />
-                         <Separator />
-                         <Separator />
+                         <ArrowDown className="absolute ms-1 mt-1 stroke-border dark:stroke-gray-600"/>
+                         {stairs()}
                     </div>
                     <div className="border-r-2 border-dashed grid gap-2 my-1">
-                         <div className="absolute grid ">
-                              <ArrowUp className="ms-1 mt-1 stroke-border dark:stroke-white"/>
-                              <div className="border w-px h-5 mx-auto border-white"></div>
-                         </div>
-                         <Separator />
-                         <Separator />
-                         <Separator />
-                         <Separator />
-                         <Separator />
+                         <ArrowUp className="absolute ms-1 mt-1 stroke-border dark:stroke-gray-600"/>
+                         {stairs()}
                     </div>
                     <div className="grid gap-2 py-1">
-                         <ArrowDown className="absolute ms-1 mt-1 stroke-black/50 dark:stroke-white"/>
-                         <Separator />
-                         <Separator />
-                         <Separator />
-                         <Separator />
-                         <Separator />
+                         <ArrowDown className="absolute ms-1 mt-1 stroke-border dark:stroke-gray-600"/>
+                         {stairs()}
                     </div>
                     <div className="grid gap-2 py-1 border-l-2">
-                         <ArrowUp className="absolute ms-1 mt-1 stroke-border"/>
-                         <Separator />
-                         <Separator />
-                         <Separator />
-                         <Separator />
-                         <Separator />
+                         <ArrowUp className="absolute ms-1 mt-1 stroke-border dark:stroke-gray-600"/>
+                         {stairs()}
                     </div>
                </div>
-               <div className="grid grid-cols-2">
-                    <div className="ps-3 pe-4">
-                         <div className="border mt-2 dark:border-white"></div>
+          </div>
+     )
+}
+
+export function EmergencyStairs({inverse} : {inverse: boolean}) {
+     const stairs = () => {
+          return (
+               <>
+                    {Array.from({length: 6}, () => (
+                         <Separator orientation="vertical" />
+                    ))}
+               </>
+          )
+     }
+
+     return (
+          <div className={`border-5 ${inverse ? "border-r-0" : "border-l-0"} border-b-5 h-20 w-20`}>
+               <div className={`grid h-full ${inverse ? 'ps-4' : 'pe-4'}`}>
+                    <div className="flex justify-between px-1">
+                         <ArrowLeft className="absolute ms-3 mt-1 stroke-border dark:stroke-gray-600"/>
+                         {stairs()}
                     </div>
-                    <div className="ps-3 pe-4">
-                         <Separator className="mt-2" />
+                    <div className="flex justify-between px-1 border-t-2">
+                         <ArrowRight className="absolute ms-3 mt-1 stroke-border dark:stroke-gray-600"/>
+                         {stairs()}
                     </div>
                </div>
           </div>
--- resources/js/pages/rooms/index.tsx
+++ resources/js/pages/rooms/index.tsx
@@ -1,10 +1,12 @@
-import Stairs from '@/components/rooms/stairs';
+import { CenterStairs, EmergencyStairs } from '@/components/rooms/stairs';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { Separator } from '@/components/ui/separator';
+import { useSidebar } from '@/components/ui/sidebar';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Room } from '@/types/models';
-import { Head } from '@inertiajs/react';
+import { Head, usePage } from '@inertiajs/react';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,53 +18,71 @@ const breadcrumbs: BreadcrumbItem[] = [
 export default function RoomIndex({rooms}: {rooms: Room[]}) {
     const wSpan: any = {
         "1": "w-24",
-        "1.5": "w-30",
-        "2": "w-40"
+        "1.5": "w-36",
+        "2": "w-44",
     };
 
     const hSpan: any = {
         "1": "h-24",
-        "1.5": "h-30",
-        "2": "h-40"
+        "1.5": "h-36",
+        "2": "h-44"
     };
+
+    const { sidebarOpen } = usePage().props;
+    // const { state } = useSidebar();
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Rooms"/>
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="text-center">
-                    <h1 className='text-2xl font-semibold text-center'>Floor 3</h1>
-                    <div className="flex gap-2 justify-center h-6 mt-2">
-                        <h2>MONDAY</h2>
-                        <Separator orientation='vertical'/>
-                        <h2>2:30 PM - 4:00 PM</h2>
-                    </div>
-                </div>
-                <div className="flex-1 flex flex-col justify-center">
-                    <div className="flex mx-auto">
-                        <div>
-                            {rooms.filter(room => room.placement == "left").map(room => (
-                                <div className={`border-5 border-b-0 last:border-b-5 ${hSpan[room.span]} w-20`}>
-                                    <h5 className='text-xs text-center flex flex-col justify-center h-full'>{room.code}</h5>
-                                </div>
-                            ))}
-                        </div>
-                        <div className='flex'>
-                            {rooms.filter(room => room.placement == "center").map(room => room.code == "Stairs" ? <Stairs/> :(
-                                <div className={`border-5 border-l-0 last:border-r-0 h-20 ${wSpan[room.span]}`}>
-                                    <h5 className='text-xs text-center flex flex-col justify-center h-full'>{room.type}</h5>
-                                </div>
-                            ))}
-                        </div>
-                        <div>
-                            {rooms.filter(room => room.placement == "right").map(room => (
-                                <div className={`border-5 border-b-0 last:border-b-5 ${hSpan[room.span]} w-20`}>
-                                    <h5 className='text-xs text-center flex flex-col justify-center h-full'>{room.code}</h5>
-                                </div>
-                            ))}
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl">
+                <div className="text-center absolute w-full flex justify-center pt-4 z-50">
+                    <div className="dark:bg-background/40 w-96 p-10 rounded-2xl backdrop-blur-2xl">
+                        <h1 className='text-2xl font-semibold text-center'>Floor 3</h1>
+                        <div className="flex gap-2 justify-center h-6 mt-2">
+                            <h2>MONDAY</h2>
+                            <Separator orientation='vertical'/>
+                            <h2>2:30 PM - 4:00 PM</h2>
                         </div>
                     </div>
                 </div>
+                <TransformWrapper smooth={true}>
+                    <TransformComponent>
+                        <div className={` ${sidebarOpen ? 'h-[calc(100vh-5rem)] w-[calc(100vw-16rem)]' :  'h-[calc(100vh-4rem)] w-[calc(100vw-4rem)]'} flex flex-col justify-center`}>
+                            <div className="flex mx-auto">
+                                <div>
+                                    {rooms.filter(room => room.placement == "left").map(room => (
+                                        <div className={`border-5 border-b-0 ${hSpan[room.span]} w-20`}>
+                                            <h5 className='text-xs text-center flex flex-col justify-center h-full'>{room.code}</h5>
+                                        </div>
+                                    ))}
+                                    <EmergencyStairs inverse={true} />
+                                </div>
+                                <div className="flex flex-col">
+                                    <div className='flex'>
+                                        {rooms.filter(room => room.placement == "center").map(room => room.code == "Stairs" ? <CenterStairs/> :(
+                                            <div className={`border-5 border-l-0 last:border-r-0 h-20 ${wSpan[room.span]}`}>
+                                                <h5 className='text-xs text-center flex flex-col justify-center h-full'>{room.code}</h5>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className='w-full flex-1 flex pt-8'>
+                                        <div className='w-8 border-b-5' />
+                                        <div className='flex-1 border-5 rounded-t-2xl border-b-0' />
+                                        <div className='w-8 border-b-5' />
+                                    </div>
+                                </div>
+                                <div className=''>
+                                    {rooms.filter(room => room.placement == "right").map(room => (
+                                        <div className={`border-5 border-b-0 ${hSpan[room.span]} w-20`}>
+                                            <h5 className='text-xs text-center flex flex-col justify-center h-full'>{room.code}</h5>
+                                        </div>
+                                    ))}
+                                    <EmergencyStairs inverse={false} />
+                                </div>
+                            </div>
+                        </div>
+                    </TransformComponent>
+                </TransformWrapper>
             </div>
         </AppLayout>
     );
